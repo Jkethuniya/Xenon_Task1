0d21693f61e1866cfc470a18bfec0f3f16e1b7dc
Jkethuniya <jatin.2022ca039@mnnit.ac.in>
2024-10-22 19:32:12 +0530

    add deployed urls

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,7 +11,7 @@ export const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://xenon-task1-ten.vercel.app",
     optionsSuccessStatus: 200,
     preflightContinue: false,
     methods: "GET,POST,OPTIONS",
