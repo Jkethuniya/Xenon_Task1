41b745f8768fc18eb491822d70132eebd74242e6
Jkethuniya <jatin.2022ca039@mnnit.ac.in>
2024-10-22 20:01:29 +0530

    comment console

--- backend/src/app/middlewares/Auth.js
+++ backend/src/app/middlewares/Auth.js
@@ -6,17 +6,17 @@ const AppRole = [ "Admin"]
 export const isAuth = (...roles)=> async (req, res, next)=>{
 
 
-console.log(roles)
+// console.log(roles)
 
 const token  = req.headers["authorization"]?.split("Bearer ")[1];
-if(token) console.log(token)
+if(token) {}
     else res.status(401).json("token not found") 
 
 const user = await verifyToken(token);
-console.log(user)
+// console.log(user)
 if(!user) res.status(401).json("invaild token") 
 req.user = {id:user.id , role:user?.role}
-console.log("authenticated succes")
+// console.log("authenticated succes")
 next();
     
 }
