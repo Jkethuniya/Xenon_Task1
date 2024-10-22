2cfea2f26d12dafa87a25cf278ae7f27077b8f8b
Jkethuniya <jatin.2022ca039@mnnit.ac.in>
2024-10-22 19:48:56 +0530

    changes made at properties

--- frontend/src/components/Properties.jsx
+++ frontend/src/components/Properties.jsx
@@ -5,30 +5,7 @@ import PropertyDetail from './PropertyDetail';
 import { useNavigate } from 'react-router-dom';
 import { properties } from '../utils/data';
 import { recommend } from '../react-query/api/peroperty';
-// const properties = [
-//   {
-//     id: 1,
-//     title: "Modern Family Home",
-//     location: "Los Angeles, CA",
-//     price: "$1,200,000",
-//     image: "https://via.placeholder.com/300",
-//   },
-//   {
-//     id: 2,
-//     title: "Luxury Villa",
-//     location: "Miami, FL",
-//     price: "$3,500,000",
-//     image: "https://via.placeholder.com/300",
-//   },
-//   {
-//     id: 3,
-//     title: "Cozy Cottage",
-//     location: "Aspen, CO",
-//     price: "$850,000",
-//     image: "https://via.placeholder.com/300",
-//   },
-//   // Add more properties as needed
-// ];
+
 
 const Properties = () => {
     const navigate = useNavigate();
@@ -56,18 +33,7 @@ const Properties = () => {
      
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
         {properties.map((property) => (
-          // <div key={property.id} className="bg-white shadow-lg rounded-lg overflow-hidden">
-          //   <img src={property.image} alt={property.title} className="w-full h-48 object-cover" />
-          //   <div className="p-4">
-          //     <h2 className="text-2xl font-bold mb-2">{property.title}</h2>
-          //     <p className="text-gray-600">{property.location}</p>
-          //     <p className="text-gray-800 mt-2">{property.price}</p>
-      
-          //       <button onClick={() => handleViewDetails(property)}  className="mt-4 w-full py-2 px-4 bg-blue-600 text-white font-semibold rounded-md shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-          //         View Details
-          //       </button>
-          //   </div>
-          // </div>
+  
           <div key={property.id} className="bg-white rounded-xl overflow-hidden shadow-lg hover:shadow-2xl transform transition-all duration-300 hover:scale-105">
   <div className="relative">
     <img 
